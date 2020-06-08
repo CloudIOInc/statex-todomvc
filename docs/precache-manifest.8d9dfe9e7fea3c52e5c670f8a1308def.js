f0ea02a6eb99202ee1e44846109031ad54ddd977
Sreeni <sreeni@cloudio.io>
2020-06-07 20:48:59 -0700

    docs

---
+++ docs/precache-manifest.8d9dfe9e7fea3c52e5c670f8a1308def.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e46c58558307683cb76c65bf967a7754",
+    "url": "/statex-todomvc/index.html"
+  },
+  {
+    "revision": "c169ffedc4e284a08fbf",
+    "url": "/statex-todomvc/static/css/2.85efaca7.chunk.css"
+  },
+  {
+    "revision": "c169ffedc4e284a08fbf",
+    "url": "/statex-todomvc/static/js/2.902cabfd.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/statex-todomvc/static/js/2.902cabfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da9703d1848f9f3b6a77",
+    "url": "/statex-todomvc/static/js/main.76ec4141.chunk.js"
+  },
+  {
+    "revision": "de41be6bd582f2ac4db8",
+    "url": "/statex-todomvc/static/js/runtime-main.7e17371e.js"
+  }
+]);
